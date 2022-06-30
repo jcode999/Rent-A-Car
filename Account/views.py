73aad4c80589536624bba2ea7385fe43e03bcbf4
Jigme Sherpa <jigmesherpa@my.unt.edu>
2022-06-30 02:05:47 -0500

    first image (logo) parsed in home

--- Account/views.py
+++ Account/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def testFun(request):
-    return render(request, 'index.html')
+def testFun():
+    pass
--- Home/views.py
+++ Home/views.py
@@ -4,6 +4,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def logoDisplay(request):
-    html = "<html><body><H2> Rent-A-Car <H2/> <H2> Test For User Acount </H2> </body></head>"
-    return HttpResponse(html)
+    return render(request, 'index.html')
     
