fe6f958c906da36b67a4e845d6bb108ce2bf4a83
Zac <ZacharyChenausky@my.unt.edu>
2022-07-13 06:29:46 -0500

    heoroku settings

--- RentACar/settings/prod.py
+++ RentACar/settings/prod.py
@@ -9,3 +9,36 @@ DEBUG = False
 ALLOWED_HOST = [
   'UNT_rentacar.herokuapp.com',
 ]
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+LOGGING = {
+  'version': 1,
+  'disable_existing_loggers': False,
+  'formatters': {
+    'verbose': {
+      'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+      'datefmt' : "%d/%b/%Y %H:%M:%S"
+    },
+    'simple': {
+      'format' : '%(levelname)s %(message)s'
+    },
+  },
+  'handlers': {
+    'console': {
+      'level': 'DEBUG',
+      'class': 'logging.StreamHandler',
+    },
+  },
+  'loggers': {
+    'MYAPP': {
+      'handlers': ['console'],
+      'level' : 'DEBUG',
+    },
+  }
+}
+
+
+# Heroku Settings
+django_on_heroku.settings(locals(), staticfiles=False)
+del DATABASES['default']['OPTIONS']['sslmode']
