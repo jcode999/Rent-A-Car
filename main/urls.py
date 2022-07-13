e65a93b4c5d2350c110d9733ba4c92c40e0cd968
Zac Chenausky <zchenausky@gmail.com>
2022-07-13 00:57:46 -0500

    update files

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
-from django.urls import re_path
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("cars/", views.car, name="car"),
 ]
--- main/views.py
+++ main/views.py
@@ -4,4 +4,15 @@ from django.http import HttpResponse
 
 
 def index(response):
-    return HttpResponse("<h1>Hello</h1>")
+    return HttpResponse("<h1>Hello This is the Home page!</h1>")
+
+
+#def car(response):
+ #   return HttpResponse("<h1> {{ name }} </h1>")
+
+def car(request):
+    name = "This is the cars home page"
+    return render(request,
+    'home.html', {
+        "name": name
+    })
