800b5a7542bd27120cde7fa112151cb3f15c86a6
Zac Chenausky <zchenausky@gmail.com>
2022-07-13 01:59:23 -0500

    added menu bar and url functionality

--- main/views.py
+++ main/views.py
@@ -3,16 +3,16 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def index(response):
-    return HttpResponse("<h1>Hello This is the Home page!</h1>")
-
-
-#def car(response):
- #   return HttpResponse("<h1> {{ name }} </h1>")
+def index(request):
+    top = "This is the main page"
+    return render(request,
+    'home.html', {
+        "top": top
+    })
 
 def car(request):
     name = "This is the cars home page"
     return render(request,
-    'home.html', {
+    'carshome.html', {
         "name": name
     })
