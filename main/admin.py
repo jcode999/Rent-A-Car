c7ae1ce14ddbe24b23495c622d6ff335cfdf9519
Zac Chenausky <zchenausky@gmail.com>
2022-07-13 02:40:14 -0500

    Added class objects Vehicle and User

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Vehicle
+from .models import RentACarUser
 
+admin.site.register(Vehicle)
+admin.site.register(RentACarUser)
 # Register your models here.
--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Vehicle(models.Model):
+    make = models.CharField('Vehicle Make', max_length=50)
+    mod = models.CharField('Vehicle Model', max_length=50)
+    year = models.CharField('Vehicle Year', max_length=50)
+    description = models.TextField(blank=True)
+
+def __str__(self):
+    return self.name
+
+class RentACarUser(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField('User Email')
+
+def __str__(self):
+    return self.first_name + ' ' + self.last_name
+
