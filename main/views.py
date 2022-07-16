2b2e767a330baf1ce8234e649f9437cc70bd5038
Zac <ZacharyChenausky@my.unt.edu>
2022-07-16 18:37:39 -0500

    cars page html look

--- main/views.py
+++ main/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Vehicle
 # Create your views here.
 
 
 def index(request):
     top = "This is the main page"
     return render(request,
-    'home.html', {
-        "top": top
-    })
+                  'home.html', {
+                      "top": top
+                  })
+
 
 def car(request):
-    name = "This is the cars home page"
+    vehicle_list = Vehicle.objects.all()
     return render(request,
-    'carshome.html', {
-        "name": name
-    })
+                  'carshome.html',
+                  {
+                      'vehicle_list': vehicle_list
+                  })
