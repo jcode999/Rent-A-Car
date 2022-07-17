998b8a35da5ab2d367de730ab9e484197b091f2f
Jigme Sherpa <jigmesherpa@my.unt.edu>
2022-07-17 10:38:19 -0500

    hosting on heroku failed, last update caused server issue, removed last changes

--- RentACar/settings.py
+++ RentACar/settings.py
@@ -121,7 +121,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
