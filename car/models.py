33afb101ba90282ea39542d3f2c44dbf4c5d6d51
Zachary <zchenausky@gmil.com>
2022-07-20 21:13:44 -0500

    updated website withworking cars page

--- car/models.py
+++ car/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Vehicle(models.Model):
+    make = models.CharField('Vehicle Make', max_length=50)
+    mod = models.CharField('Vehicle Model', max_length=50)
+    year = models.CharField('Vehicle Year', max_length=50)
+    price = models.CharField('Vehicle Price', max_length=50, default=0)
+    description = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to="images/", default="images/default.png")
+    slug = models.SlugField(max_length=255, default=0)
+
+
+def get_absolute_url(self):
+    return reverse("main:single", args=[self.slug])
--- car/views.py
+++ car/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Vehicle
 # Create your views here.
+
+
+def home(request):
+    message = "This is the main page"
+    return render(request,
+                  'home.html', {
+                      "message": message
+                  })
+
+
+def car(request):
+    vehicle_list = Vehicle.objects.all()
+    return render(request,
+                  'carshome.html',
+                  {
+                      'vehicle_list': vehicle_list
+                  })
+
+
+def single(request, slug):
+    data = Vehicle.objects.get(slug=slug)
+    return render(request, "single.html", {"vehicle": data})
