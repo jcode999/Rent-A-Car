70e5f1b7c6193509e836b0c931294e96d9f1e0af
Jigme Sherpa <jigmesherpa@my.unt.edu>
2022-07-25 15:10:46 -0500

    test

--- car/admin.py
+++ car/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Vehicle, Reservation
+from .models import Vehicle
 from account.models import Account
 
 
 admin.site.register(Vehicle)
 admin.site.register(Account)
-admin.site.register(Reservation)
+
 # Register your models here.
