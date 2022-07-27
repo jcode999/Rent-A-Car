c69e01275e55c9110b7345e645407fa56255d1f5
Zac <ZacharyChenausky@my.unt.edu>
2022-07-27 14:25:54 -0500

    update

--- Account/views.py
+++ Account/views.py
@@ -1,18 +1,20 @@
 
 from django.shortcuts import render, redirect
-
 from account.models import Account
 from .forms import LogInForm, RegistrationForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
 def dashboard(request, username):
     user = Account.objects.get(username=username)
-    return render(request, 'dashboard/dashboard.html', {'user': user})
+    if request.user.is_authenticated:
+        return render(request, 'dashboard/dashboard.html', {'user': user})
+    else:
+        return render(request, 'invalidpage.html')
 
 
 def logIn(request):
@@ -44,3 +46,9 @@ def registration(response):
         form = RegistrationForm()
 
     return render(response, 'registration/registration.html', {'registration_form': form})
+
+
+def log_out(request):
+    logout(request)
+    messages.success(request, ("You are logged out"))
+    return redirect('account:login')
--- RentACar/settings.py
+++ RentACar/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 ROOT_URLCONF = 'RentACar.urls'
 
