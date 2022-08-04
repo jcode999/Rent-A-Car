58e8137ab7e6ef8aae54e6cc43a8ec5fd6a3c2f2
Jigme Sherpa <jigmesherpa@my.unt.edu>
2022-08-04 13:02:33 -0500

    account updates

--- Account/urls.py
+++ Account/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path('login/', views.logIn, name='login'),
     path('registration/', views.registration, name='registration'),
-    path('dashboard/<username>/', views.dashboard, name='dashboard'),
-    path('logout/', views.log_out,name='logout')
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('logout/', views.log_out, name='logout'),
+    path('dashboard/payment', views.add_credit_card, name='add_payment'),
+    path('dashboard/update', views.update_info, name='update_info'),
 
 ]
