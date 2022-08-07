ce055652d016f538e0a916d96478d8ce784785c2
Zac <ZacharyChenausky@my.unt.edu>
2022-08-06 23:03:55 -0500

    updated settings and requirements for heroku

--- RentACar/settings.py
+++ RentACar/settings.py
@@ -7,9 +7,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import django_on_heroku
 import os
 from pathlib import Path
-#import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,5 +137,5 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-# django_on_heroku.settings(locals())
+# Configure Django App for Heroku
+django_on_heroku.settings(locals())
