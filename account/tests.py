3084f957103d69ad2182743d59fc51965a3b7ba8
Jigme Sherpa <jigmesherpa@my.unt.edu>
2022-08-08 11:02:25 -0500

    log in page accessibility

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+.form-control{
+    display: block;
+    width: 75%;
+    padding: .375rem .75rem;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #000205;
+    background-color: rgb(245, 246, 241);
+    background-clip: padding-box;
+    border: 1px solid #2e20f0;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    border-radius: .375rem;
+
+    
+    
+}
