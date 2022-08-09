c8d2b95c1ace0418e627445a474a0a7d48119aff
Zac <ZacharyChenausky@my.unt.edu>
2022-08-09 01:05:18 -0500

    update photos

--- RentACar/settings.py
+++ RentACar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-eo@71e$48x90j8k!b#kqnza00z+kiue%@$!e+m^=r1mbuaf2@7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'rentacarunt.herokuapp.com']
 
